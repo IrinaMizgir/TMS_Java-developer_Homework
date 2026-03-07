a70a0ace59cb217d41330eb93c22a6e6ec8f98fe
IrinaMizgir <ira.mizgir797@gmail.com>
2026-03-07 20:09:36 +0300

    домашнее задание урок 2

---
+++ Lesson2/src/Exercise3.java
@@ -0,0 +1,15 @@
+/**
+ * @author Ирина Мизгир
+ * @date 03.03.2026 19:12
+ */
+//Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно.
+public class Exercise3 {
+    public static void main(String[] args) {
+        System.out.println("Квадраты чисел от 10 до 20:");
+
+        for (int i = 10; i <= 20; i++) {
+            int square = i * i;
+            System.out.println(i + "² = " + square);
+        }
+    }
+}
---
+++ Lesson2/src/Exercise5.java
@@ -0,0 +1,48 @@
+/**
+ * @author Ирина Мизгир
+ * @date 03.03.2026 19:39
+ */
+//Напишите программу, где пользователь вводит любое целое положительное число. А
+//программа суммирует все числа от 1 до введенного пользователем числа. Для ввода
+//числа воспользуйтесь классом Scanner. Сделать проверку, чтобы пользователь не мог
+//ввести некорректные данные.
+
+import java.util.Scanner;
+
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        boolean validInput = false;
+
+        // Ввод числа с проверкой корректности
+        while (!validInput) {
+            System.out.print("Введите целое положительное число: ");
+
+            if (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+
+                if (number > 0) {
+                    validInput = true;
+                } else {
+                    System.out.println("Ошибка: число должно быть положительным. Попробуйте снова.");
+                }
+            } else {
+                System.out.println("Ошибка: введено не целое число. Попробуйте снова.");
+                scanner.next(); // Очистка некорректного ввода
+            }
+        }
+
+        // Вычисление суммы чисел от 1 до введенного числа
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+        }
+
+        // Вывод результата
+        System.out.println("Сумма чисел от 1 до " + number + " = " + sum);
+
+        scanner.close();
+    }
+}
